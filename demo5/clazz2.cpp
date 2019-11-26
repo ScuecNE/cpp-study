eee24d7728f6e529fb191b8d141632d79b18e2d5
suguswong <suguswong@outlook.com>
2019-11-26 16:11:25 +0800

    新增拷贝构造函数

--- demo5/clazz2.cpp
+++ demo5/clazz2.cpp
@@ -6,12 +6,14 @@ class Box {
 
     public:
         Box(double l, double w, double h);
+        Box(const Box &box);
         ~Box();
         double getVolume();
     private:
         double length;
         double width;
         double heigth;
+        int *ptr;
 };
 
 Box::Box(double l, double w, double h) : length(l), width(w), heigth(h) {
@@ -19,10 +21,20 @@ Box::Box(double l, double w, double h) : length(l), width(w), heigth(h) {
     cout << "length:" << length << endl;
     cout << "width:" << width << endl;
     cout << "heigth:" << heigth << endl;
+
+    // 为指针分配内存
+    ptr = new int;
+    *ptr = length;
+}
+
+Box::Box(const Box &box) {
+    ptr = new int;
+    *ptr = *box.ptr;
 }
 
 Box::~Box(void) {
     cout << "关闭文件、释放内存" << endl;
+    delete ptr;
 }
 
 double Box::getVolume() {
