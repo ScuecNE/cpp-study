197a4f6b05d3fd69a997e81c620eab5994c49efe
suguswong <suguswong@outlook.com>
2019-11-27 11:37:06 +0800

    类指针

--- demo5/clazz3.cpp
+++ demo5/clazz3.cpp
@@ -9,8 +9,10 @@ class DomainClazz {
         DomainClazz();
         ~DomainClazz();
         friend void printValue(DomainClazz domainClazz);
+        bool compare(DomainClazz domainClazz);
         friend class FriendClazz;
         void setValue(int val);
+        int getValue();
 };
 
 DomainClazz::DomainClazz() {}
@@ -21,8 +23,16 @@ void DomainClazz::setValue(int val) {
     value = val;
 }
 
+int DomainClazz::getValue() {
+    return this -> value;
+}
+
 void printValue(DomainClazz domainClazz) {
     cout << "value:" << domainClazz.value << endl;
+}
+
+bool DomainClazz::compare(DomainClazz domainClazz) {
+    return this -> getValue() > domainClazz.getValue();
 }
 
 class FriendClazz {
@@ -51,5 +61,24 @@ int main() {
     domainClazz.setValue(100);
     printValue(domainClazz);
     friendClazz.print(200, domainClazz);
+
+    DomainClazz domainClazz1;
+    domainClazz1.setValue(300);
+
+    // this指针
+    cout << "domainClazz1:" << domainClazz1.getValue() << endl;
+    cout << "domainClazz" << domainClazz.getValue() << endl;
+    if (domainClazz1.compare(domainClazz)) {
+        cout << "domainClazz1 bigger than domainClazz" << endl;
+    } else {
+        cout << "domainClazz1 smaller than domainClazz" << endl;
+    }
+
+    // 类指针
+    DomainClazz *ptr;
+    ptr = &domainClazz1;
+
+    cout << "domainClazz1.getValue():" << ptr -> getValue();
+    
     return 0;
 }
